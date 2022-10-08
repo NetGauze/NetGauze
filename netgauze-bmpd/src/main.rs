d898163321467a742bb1e6c629ca4d2f38f6305c
Ahmed El-Hassany <a.hassany@gmail.com>
2022-10-09 01:12:02 +0200

    WIP BMP play with dummy tower-rs service

--- netgauze-bmpd/src/main.rs
+++ netgauze-bmpd/src/main.rs
@@ -1,11 +1,49 @@
-use std::{io, net::SocketAddr};
-
-use futures::StreamExt;
+use futures::{
+    future::{ready, Ready},
+    StreamExt,
+};
+use std::{
+    error::Error,
+    io,
+    net::SocketAddr,
+    task::{Context, Poll},
+    time::Duration,
+};
 
 use tokio::net::TcpListener;
 use tokio_util::codec::FramedRead;
+use tower::ServiceBuilder;
 
-use netgauze_bmpd::{codec::BmpCodec, transport::TaggedFramedStream, AddrInfo};
+use netgauze_bmpd::{codec::BmpCodec, transport::TaggedFramedStream, AddrInfo, TaggedData};
+
+use netgauze_bmp_pkt::BmpMessage;
+use netgauze_bmpd::codec::BmpCodecDecoderError;
+use tower::ServiceExt;
+use tower_service::Service;
+
+#[derive(Debug, Eq, PartialEq)]
+struct DummyService;
+
+impl Service<Result<TaggedData<AddrInfo, BmpMessage>, TaggedData<AddrInfo, BmpCodecDecoderError>>>
+    for DummyService
+{
+    type Response =
+        Result<TaggedData<AddrInfo, BmpMessage>, TaggedData<AddrInfo, BmpCodecDecoderError>>;
+    type Error = Box<dyn Error + Send + Sync>;
+    type Future = Ready<Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(
+        &mut self,
+        req: Result<TaggedData<AddrInfo, BmpMessage>, TaggedData<AddrInfo, BmpCodecDecoderError>>,
+    ) -> Self::Future {
+        println!("In service {:?}", req);
+        ready(Ok(req))
+    }
+}
 
 async fn run_server(local_socket: SocketAddr) -> io::Result<()>
 where {
@@ -14,23 +52,16 @@ where {
         let (tcp_stream, remote_socket) = listener.accept().await?;
         let (rx, tx) = tcp_stream.into_split();
         let addr_info = AddrInfo::new(local_socket, remote_socket);
-        let mut bmp_stream = TaggedFramedStream::new(addr_info, FramedRead::new(rx, BmpCodec), tx);
+        let bmp_stream = TaggedFramedStream::new(addr_info, FramedRead::new(rx, BmpCodec), tx);
+
         tokio::spawn(async move {
-            while let Some(result) = bmp_stream.next().await {
-                match result {
-                    Ok(tagged_msg) => println!(
-                        "[{:?}] BMP Message: {:?}",
-                        tagged_msg.tag(),
-                        tagged_msg.value()
-                    ),
-                    Err(tagged_err) => eprintln!(
-                        "[{:?}], BMP Message Error: {:?}",
-                        tagged_err.tag(),
-                        tagged_err.value()
-                    ),
-                };
+            let mut responses = ServiceBuilder::new()
+                .rate_limit(1, Duration::from_secs(10))
+                .service(DummyService)
+                .call_all(bmp_stream);
+            while let Some(response) = responses.next().await {
+                println!("Response {:?}", response);
             }
-            println!("[{:?}] BMP Stream is closed", addr_info);
         });
     }
 }
