25c6af5b5d3cd4e525f4d1558e9de949f2c29d50
Ahmed El-Hassany <a.hassany@gmail.com>
2022-10-22 23:25:11 +0200

    WIP: ipfix automatic deser generation

--- netgauze-ipfix-code-generator/src/lib.rs
+++ netgauze-ipfix-code-generator/src/lib.rs
@@ -366,6 +366,11 @@ pub fn generate_ie_status() -> String {
     ret
 }
 
+/// Use at the beginning of ie_generated for defining custom types
+pub fn generate_common_types() -> String {
+    "pub type MacAddress = [u8; 6];\n\n".to_string()
+}
+
 /// `TryFrom` block for  InformationElementId
 fn generate_ie_try_from_pen_code(name_prefixes: &Vec<(String, String, u32)>) -> String {
     let mut ret = String::new();
@@ -590,6 +595,292 @@ fn generate_ie_value_converters(rust_type: &str, name_prefix: &String, ie_name: 
     ret
 }
 
+pub fn generate_ie_deserializer(rust_type: &str, name_prefix: &String, ie_name: &String) -> String {
+    let mut ret = String::new();
+    let ty_name = format!("{}{}", name_prefix, ie_name);
+    let mut std_error = String::new();
+
+    std_error.push_str("#[allow(non_camel_case_types)]\n");
+    std_error.push_str("#[derive(netgauze_serde_macros::LocatedError, Eq, PartialEq, Clone, Debug, serde::Serialize, serde::Deserialize)]\n");
+    std_error.push_str(format!("pub enum {}ParsingError {{\n", ty_name).as_str());
+    std_error.push_str("    #[serde(with = \"super::super::ErrorKindSerdeDeref\")]\n");
+    std_error.push_str("    NomError(#[from_nom] nom::error::ErrorKind),\n");
+    std_error.push_str("    InvalidLength(u16),\n");
+    std_error.push_str("}\n\n");
+
+    let mut header = format!("impl<'a> netgauze_parse_utils::ReadablePDUWithOneInput<'a, u16, Located{}ParsingError<'a>> for {} {{\n", ty_name, ty_name);
+    header.push_str(format!("    fn from_wire(buf: netgauze_parse_utils::Span<'a>, length: u16) -> nom::IResult<netgauze_parse_utils::Span<'a>, Self, Located{}ParsingError<'a>> {{\n", ty_name).as_str());
+
+    match rust_type {
+        "u8" => {
+            ret.push_str(std_error.as_str());
+            ret.push_str(header.as_str());
+            ret.push_str("        let (buf, value) = match length {\n");
+            ret.push_str("            1 => nom::number::complete::be_u8(buf)?,\n");
+            ret.push_str(format!("            _ => return Err(nom::Err::Error(Located{}ParsingError::new(buf, {}ParsingError::InvalidLength(length))))\n", ty_name, ty_name).as_str());
+            ret.push_str("        };\n");
+            ret.push_str(format!("        Ok((buf, {}(value)))\n", ty_name).as_str());
+            ret.push_str("    }\n");
+            ret.push_str("}\n\n");
+        }
+        "u16" => {
+            ret.push_str(std_error.as_str());
+            ret.push_str(header.as_str());
+            ret.push_str("        let (buf, value) = match length {\n");
+            ret.push_str("            1 => {\n");
+            ret.push_str(
+                "                let (buf, value) = nom::number::complete::be_u8(buf)?;\n",
+            );
+            ret.push_str("                (buf, value as u16)\n");
+            ret.push_str("            }\n");
+            ret.push_str("            2 => nom::number::complete::be_u16(buf)?,\n");
+            ret.push_str(format!("            _ => return Err(nom::Err::Error(Located{}ParsingError::new(buf, {}ParsingError::InvalidLength(length))))\n", ty_name, ty_name).as_str());
+            ret.push_str("        };\n");
+            ret.push_str(format!("        Ok((buf, {}(value)))\n", ty_name).as_str());
+            ret.push_str("    }\n");
+            ret.push_str("}\n\n");
+        }
+        "u32" => {
+            ret.push_str(std_error.as_str());
+            ret.push_str(header.as_str());
+            ret.push_str("        let (buf, value) = match length {\n");
+            ret.push_str("            1 => {\n");
+            ret.push_str(
+                "                let (buf, value) = nom::number::complete::be_u8(buf)?;\n",
+            );
+            ret.push_str("                (buf, value as u32)\n");
+            ret.push_str("            }\n");
+            ret.push_str("            2 => {\n");
+            ret.push_str(
+                "                let (buf, value) = nom::number::complete::be_u16(buf)?;\n",
+            );
+            ret.push_str("                (buf, value as u32)\n");
+            ret.push_str("            }\n");
+            ret.push_str("            4 => nom::number::complete::be_u32(buf)?,\n");
+            ret.push_str(format!("            _ => return Err(nom::Err::Error(Located{}ParsingError::new(buf, {}ParsingError::InvalidLength(length))))\n", ty_name, ty_name).as_str());
+            ret.push_str("        };\n");
+            ret.push_str(format!("        Ok((buf, {}(value)))\n", ty_name).as_str());
+            ret.push_str("    }\n");
+            ret.push_str("}\n\n");
+        }
+        "u64" => {
+            ret.push_str(std_error.as_str());
+            ret.push_str(header.as_str());
+            ret.push_str("        let (buf, value) = match length {\n");
+            ret.push_str("            1 => {\n");
+            ret.push_str(
+                "                let (buf, value) = nom::number::complete::be_u8(buf)?;\n",
+            );
+            ret.push_str("                (buf, value as u64)\n");
+            ret.push_str("            }\n");
+            ret.push_str("            2 => {\n");
+            ret.push_str(
+                "                let (buf, value) = nom::number::complete::be_u16(buf)?;\n",
+            );
+            ret.push_str("                (buf, value as u64)\n");
+            ret.push_str("            }\n");
+            ret.push_str("            4 => {\n");
+            ret.push_str(
+                "                let (buf, value) = nom::number::complete::be_u32(buf)?;\n",
+            );
+            ret.push_str("                (buf, value as u64)\n");
+            ret.push_str("            }\n");
+            ret.push_str("            8 => nom::number::complete::be_u64(buf)?,\n");
+            ret.push_str(format!("            _ => return Err(nom::Err::Error(Located{}ParsingError::new(buf, {}ParsingError::InvalidLength(length))))\n", ty_name, ty_name).as_str());
+            ret.push_str("        };\n");
+            ret.push_str(format!("        Ok((buf, {}(value)))\n", ty_name).as_str());
+            ret.push_str("    }\n");
+            ret.push_str("}\n\n");
+        }
+        "i8" => {
+            ret.push_str(std_error.as_str());
+            ret.push_str(header.as_str());
+            ret.push_str("        let (buf, value) = match length {\n");
+            ret.push_str("            1 => nom::number::complete::be_i8(buf)?,\n");
+            ret.push_str(format!("            _ => return Err(nom::Err::Error(Located{}ParsingError::new(buf, {}ParsingError::InvalidLength(length))))\n", ty_name, ty_name).as_str());
+            ret.push_str("        };\n");
+            ret.push_str(format!("        Ok((buf, {}(value)))\n", ty_name).as_str());
+            ret.push_str("    }\n");
+            ret.push_str("}\n\n");
+        }
+        "i16" => {
+            ret.push_str(std_error.as_str());
+            ret.push_str(header.as_str());
+            ret.push_str("        let (buf, value) = match length {\n");
+            ret.push_str("            1 => {\n");
+            ret.push_str(
+                "                let (buf, value) = nom::number::complete::be_i8(buf)?;\n",
+            );
+            ret.push_str("                (buf, value as i16)\n");
+            ret.push_str("            }\n");
+            ret.push_str("            2 => nom::number::complete::be_i16(buf)?,\n");
+            ret.push_str(format!("            _ => return Err(nom::Err::Error(Located{}ParsingError::new(buf, {}ParsingError::InvalidLength(length))))\n", ty_name, ty_name).as_str());
+            ret.push_str("        };\n");
+            ret.push_str(format!("        Ok((buf, {}(value)))\n", ty_name).as_str());
+            ret.push_str("    }\n");
+            ret.push_str("}\n\n");
+        }
+        "i32" => {
+            ret.push_str(std_error.as_str());
+            ret.push_str(header.as_str());
+            ret.push_str("        let (buf, value) = match length {\n");
+            ret.push_str("            1 => {\n");
+            ret.push_str(
+                "                let (buf, value) = nom::number::complete::be_i8(buf)?;\n",
+            );
+            ret.push_str("                (buf, value as i32)\n");
+            ret.push_str("            }\n");
+            ret.push_str("            2 => {\n");
+            ret.push_str(
+                "                let (buf, value) = nom::number::complete::be_i16(buf)?;\n",
+            );
+            ret.push_str("                (buf, value as i32)\n");
+            ret.push_str("            }\n");
+            ret.push_str("            4 => nom::number::complete::be_i32(buf)?,\n");
+            ret.push_str(format!("            _ => return Err(nom::Err::Error(Located{}ParsingError::new(buf, {}ParsingError::InvalidLength(length))))\n", ty_name, ty_name).as_str());
+            ret.push_str("        };\n");
+            ret.push_str(format!("        Ok((buf, {}(value)))\n", ty_name).as_str());
+            ret.push_str("    }\n");
+            ret.push_str("}\n\n");
+        }
+        "i64" => {
+            ret.push_str(std_error.as_str());
+            ret.push_str(header.as_str());
+            ret.push_str("        let (buf, value) = match length {\n");
+            ret.push_str("            1 => {\n");
+            ret.push_str(
+                "                let (buf, value) = nom::number::complete::be_i8(buf)?;\n",
+            );
+            ret.push_str("                (buf, value as i64)\n");
+            ret.push_str("            }\n");
+            ret.push_str("            2 => {\n");
+            ret.push_str(
+                "                let (buf, value) = nom::number::complete::be_i16(buf)?;\n",
+            );
+            ret.push_str("                (buf, value as i64)\n");
+            ret.push_str("            }\n");
+            ret.push_str("            4 => {\n");
+            ret.push_str(
+                "                let (buf, value) = nom::number::complete::be_i32(buf)?;\n",
+            );
+            ret.push_str("                (buf, value as i64)\n");
+            ret.push_str("            }\n");
+            ret.push_str("            8 => nom::number::complete::be_i64(buf)?,\n");
+            ret.push_str(format!("            _ => return Err(nom::Err::Error(Located{}ParsingError::new(buf, {}ParsingError::InvalidLength(length))))\n", ty_name, ty_name).as_str());
+            ret.push_str("        };\n");
+            ret.push_str(format!("        Ok((buf, {}(value)))\n", ty_name).as_str());
+            ret.push_str("    }\n");
+            ret.push_str("}\n\n");
+        }
+        "f32" => {
+            ret.push_str(std_error.as_str());
+            ret.push_str(header.as_str());
+            ret.push_str("        let (buf, value) = match length {\n");
+            ret.push_str("            1 => nom::number::complete::be_f32(buf)?,\n");
+            ret.push_str(format!("            _ => return Err(nom::Err::Error(Located{}ParsingError::new(buf, {}ParsingError::InvalidLength(length))))\n", ty_name, ty_name).as_str());
+            ret.push_str("        };\n");
+            ret.push_str(format!("        Ok((buf, {}(value)))\n", ty_name).as_str());
+            ret.push_str("    }\n");
+            ret.push_str("}\n\n");
+        }
+        "f64" => {
+            ret.push_str(std_error.as_str());
+            ret.push_str(header.as_str());
+            ret.push_str("        let (buf, value) = match length {\n");
+            ret.push_str("            1 => nom::number::complete::be_f64(buf)?,\n");
+            ret.push_str(format!("            _ => return Err(nom::Err::Error(Located{}ParsingError::new(buf, {}ParsingError::InvalidLength(length))))\n", ty_name, ty_name).as_str());
+            ret.push_str("        };\n");
+            ret.push_str(format!("        Ok((buf, {}(value)))\n", ty_name).as_str());
+            ret.push_str("    }\n");
+            ret.push_str("}\n\n");
+        }
+        "bool" => {
+            ret.push_str(std_error.as_str());
+            ret.push_str(header.as_str());
+            ret.push_str("        let (buf, value) = match length {\n");
+            ret.push_str("            1 => nom::number::complete::be_u8(buf)?,\n");
+            ret.push_str(format!("            _ => return Err(nom::Err::Error(Located{}ParsingError::new(buf, {}ParsingError::InvalidLength(length))))\n", ty_name, ty_name).as_str());
+            ret.push_str("        };\n");
+            ret.push_str(format!("        Ok((buf, {}(value != 0)))\n", ty_name).as_str());
+            ret.push_str("    }\n");
+            ret.push_str("}\n\n");
+        }
+        "super::MacAddress" => {
+            ret.push_str(std_error.as_str());
+            ret.push_str(header.as_str());
+            ret.push_str("        if length != 6 {\n");
+            ret.push_str(format!("            return Err(nom::Err::Error(Located{}ParsingError::new(buf, {}ParsingError::InvalidLength(length))));\n", ty_name, ty_name).as_str());
+            ret.push_str("        };\n");
+            ret.push_str("        let (buf, b0) = nom::number::complete::be_u8(buf)?;\n");
+            ret.push_str("        let (buf, b1) = nom::number::complete::be_u8(buf)?;\n");
+            ret.push_str("        let (buf, b2) = nom::number::complete::be_u8(buf)?;\n");
+            ret.push_str("        let (buf, b3) = nom::number::complete::be_u8(buf)?;\n");
+            ret.push_str("        let (buf, b4) = nom::number::complete::be_u8(buf)?;\n");
+            ret.push_str("        let (buf, b5) = nom::number::complete::be_u8(buf)?;\n");
+            ret.push_str(
+                format!("        Ok((buf, {}([b0, b1, b2, b3, b4, b5])))\n", ty_name).as_str(),
+            );
+            ret.push_str("    }\n");
+            ret.push_str("}\n\n");
+        }
+        "String" => {
+            let mut string_error = String::new();
+            std_error.push_str("#[allow(non_camel_case_types)]\n");
+            string_error.push_str("#[derive(netgauze_serde_macros::LocatedError, Eq, PartialEq, Clone, Debug, serde::Serialize, serde::Deserialize)]\n");
+            string_error.push_str(format!("pub enum {}ParsingError {{\n", ty_name).as_str());
+            string_error.push_str("    #[serde(with = \"super::super::ErrorKindSerdeDeref\")]\n");
+            string_error.push_str("    NomError(#[from_nom] nom::error::ErrorKind),\n");
+            string_error.push_str("    FromUtf8Error(String),\n");
+            string_error.push_str("}\n\n");
+
+            string_error.push_str("impl<'a> nom::error::FromExternalError<netgauze_parse_utils::Span<'a>, std::string::FromUtf8Error>\n");
+            string_error.push_str(format!("for Located{}ParsingError<'a>\n", ty_name).as_str());
+            string_error.push_str("{\n");
+            string_error.push_str("    fn from_external_error(input: netgauze_parse_utils::Span<'a>, _kind: nom::error::ErrorKind, error: std::string::FromUtf8Error) -> Self {\n");
+            string_error
+                .push_str(format!("        Located{}ParsingError::new(\n", ty_name).as_str());
+            string_error.push_str("            input,\n");
+            string_error.push_str(
+                format!(
+                    "            {}ParsingError::FromUtf8Error(error.to_string()),\n",
+                    ty_name
+                )
+                .as_str(),
+            );
+            string_error.push_str("        )\n");
+            string_error.push_str("    }\n");
+            string_error.push_str("}\n");
+
+            ret.push_str(string_error.as_str());
+            ret.push_str(header.as_str());
+
+            ret.push_str("        let (buf, value) =\n");
+            ret.push_str("            nom::combinator::map_res(nom::bytes::complete::take(length), |x: netgauze_parse_utils::Span<'_>| {\n");
+            ret.push_str("                String::from_utf8(x.to_vec())\n");
+            ret.push_str("            })(buf)?;\n");
+            ret.push_str(format!("        Ok((buf, {}(String::from(value))))\n", ty_name).as_str());
+            ret.push_str("    }\n");
+            ret.push_str("}\n");
+        }
+        "chrono::DateTime<chrono::Utc>" => {}
+        "std::net::Ipv4Addr" => {}
+        "std::net::Ipv6Addr" => {}
+        "Vec<u8>" => {}
+        ty => todo!("Unsupported deserialization for type: {}", ty),
+    }
+    ret
+}
+
+pub fn generate_ie_deserializers(name_prefix: &String, ies: &Vec<InformationElement>) -> String {
+    let mut ret = String::new();
+    ret.push_str(format!("use crate::ie::{}::*;\n\n", "iana").as_str());
+    for ie in ies {
+        let rust_type = get_rust_type(&ie.data_type);
+        ret.push_str(generate_ie_deserializer(&rust_type, name_prefix, &ie.name).as_str());
+    }
+    ret
+}
+
 fn generate_records_enum(name_prefix: &String, ies: &Vec<InformationElement>) -> String {
     let mut ret = String::new();
     ret.push_str("#[allow(non_camel_case_types)]\n");
@@ -616,7 +907,7 @@ fn get_rust_type(data_type: &str) -> String {
         "float32" => "f32",
         "float64" => "f64",
         "boolean" => "bool",
-        "macAddress" => "[u8; 6]",
+        "macAddress" => "super::MacAddress",
         "string" => "String",
         "dateTimeSeconds" => "chrono::DateTime<chrono::Utc>",
         "dateTimeMilliseconds" => "chrono::DateTime<chrono::Utc>",
@@ -624,9 +915,9 @@ fn get_rust_type(data_type: &str) -> String {
         "dateTimeNanoseconds" => "chrono::DateTime<chrono::Utc>",
         "ipv4Address" => "std::net::Ipv4Addr",
         "ipv6Address" => "std::net::Ipv6Addr",
-        "basicList" => "String",
-        "subTemplateList" => "String",
-        "subTemplateMultiList" => "String",
+        "basicList" => "Vec<u8>",
+        "subTemplateList" => "Vec<u8>",
+        "subTemplateMultiList" => "Vec<u8>",
         other => todo!("Implement rust data type conversion for {}", other),
     };
     rust_type.to_string()
