33d82a57ef490187373c7620ab7b9868643f9de0
Ahmed Elhassany <a.hassany@gmail.com>
2024-01-12 21:32:23 +0100

    Update bgp and bmp fuzzers with the new APIs

--- fuzz/fuzz_targets/fuzz_bmp_pkt.rs
+++ fuzz/fuzz_targets/fuzz_bmp_pkt.rs
@@ -20,18 +20,59 @@ use std::collections::HashMap;
 
 use libfuzzer_sys::fuzz_target;
 
+use netgauze_bgp_pkt::wire::deserializer::BgpParsingContext;
 use netgauze_bmp_pkt::{BmpMessage, PeerKey};
 use netgauze_iana::address_family::AddressType;
-use netgauze_parse_utils::{ReadablePduWithTwoInputs, Span};
+use netgauze_parse_utils::{ReadablePduWithOneInput, Span};
 
+// We don't pass BgpParsingContext as fuzzed input since we don't want to generate BgpParsingContext::parsing_errors.
 fuzz_target!(|data: (
     &[u8],
-    HashMap<PeerKey, HashMap<AddressType, u8>>,
-    HashMap<PeerKey, HashMap<AddressType, bool>>
+    HashMap<
+        PeerKey,
+        (
+            bool,
+            HashMap<AddressType, u8>,
+            HashMap<AddressType, bool>,
+            bool,
+            bool,
+            bool,
+            bool
+        ),
+    >,
 )| {
-    let (mut buf, multiple_labels, addpath) = data;
-    while let Ok((retbuf, _msg)) = BmpMessage::from_wire(Span::new(buf), &multiple_labels, &addpath)
-    {
+    let (mut buf, ctx_params) = data;
+    let mut ctx = ctx_params
+        .iter()
+        .map(
+            |(
+                k,
+                (
+                    asn4,
+                    multiple_labels,
+                    add_path,
+                    fail_on_non_unicast_withdraw_nlri,
+                    fail_on_non_unicast_update_nlri,
+                    fail_on_capability_error,
+                    fail_on_malformed_path_attr,
+                ),
+            )| {
+                (
+                    *k,
+                    BgpParsingContext::new(
+                        *asn4,
+                        multiple_labels.clone(),
+                        add_path.clone(),
+                        *fail_on_non_unicast_withdraw_nlri,
+                        *fail_on_non_unicast_update_nlri,
+                        *fail_on_capability_error,
+                        *fail_on_malformed_path_attr,
+                    ),
+                )
+            },
+        )
+        .collect();
+    while let Ok((retbuf, _msg)) = BmpMessage::from_wire(Span::new(buf), &mut ctx) {
         buf = retbuf.fragment();
     }
 });
