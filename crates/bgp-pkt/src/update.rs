d45c0a79e31c4633f05ba5bb6224b4f4d0d1ceb9
Ahmed Elhassany <a.hassany@gmail.com>
2024-01-12 20:50:03 +0100

    Add End-of-RIB (RoE) check to update message

--- crates/bgp-pkt/src/update.rs
+++ crates/bgp-pkt/src/update.rs
@@ -16,9 +16,10 @@
 //! Representations for BGP Update message
 
 use crate::nlri::Ipv4UnicastAddress;
+use netgauze_iana::address_family::AddressType;
 use serde::{Deserialize, Serialize};
 
-use crate::path_attribute::PathAttribute;
+use crate::path_attribute::{MpUnreach, PathAttribute, PathAttributeValue};
 
 /// UPDATE messages are used to transfer routing information between BGP peers
 /// as defined by [RFC4271](https://datatracker.ietf.org/doc/html/RFC4271).
@@ -68,5 +69,244 @@ impl BgpUpdateMessage {
     #[inline]
     pub const fn nlri(&self) -> &Vec<Ipv4UnicastAddress> {
         &self.nlri
+    }
+
+    /// Return address family of End-Of-RIB (EoR) messages or `None` if the
+    /// update message is not EoR
+    ///
+    /// RFC4724: An UPDATE message with no reachable Network Layer Reachability
+    /// Information (NLRI) and empty withdrawn NLRI is specified as the End-
+    /// of-RIB marker that can be used by a BGP speaker to indicate to its
+    /// peer the completion of the initial routing update after the session
+    /// is established. For the IPv4 unicast address family, the End-of-RIB
+    /// marker is an UPDATE message with the minimum length [RFC4271](https://datatracker.ietf.org/doc/html/RFC4271).
+    /// For any other address family, it is an UPDATE message that contains only
+    /// the MP_UNREACH_NLRI attribute [RFC4760](https://datatracker.ietf.org/doc/html/RFC4760)
+    /// with no withdrawn routes for that <AFI, SAFI>.
+    pub fn end_of_rib(&self) -> Option<AddressType> {
+        if !self.nlri.is_empty() || !self.withdrawn_routes.is_empty() {
+            return None;
+        }
+        if self.path_attributes.is_empty() {
+            return Some(AddressType::Ipv4Unicast);
+        }
+        let mut current = None;
+        let mut mp_unreach_count = 0;
+        for attr in &self.path_attributes {
+            if let PathAttributeValue::MpReach(_) = attr.value() {
+                return None;
+            }
+            if let PathAttributeValue::MpUnreach(unreach) = attr.value() {
+                mp_unreach_count += 1;
+                if mp_unreach_count > 1 {
+                    // Only one MpUnreach is used to indicate End-of-RIB (EoR), more than one
+                    // MpUnreach attribute doesn't define EoR.
+                    return None;
+                }
+                match unreach {
+                    MpUnreach::Ipv4Unicast { nlri } => {
+                        if nlri.is_empty() {
+                            current = Some(AddressType::Ipv4Unicast);
+                        }
+                    }
+                    MpUnreach::Ipv4Multicast { nlri } => {
+                        if nlri.is_empty() {
+                            current = Some(AddressType::Ipv4Multicast);
+                        }
+                    }
+                    MpUnreach::Ipv4NlriMplsLabels { nlri } => {
+                        if nlri.is_empty() {
+                            current = Some(AddressType::Ipv4NlriMplsLabels);
+                        }
+                    }
+                    MpUnreach::Ipv4MplsVpnUnicast { nlri } => {
+                        if nlri.is_empty() {
+                            current = Some(AddressType::Ipv4MplsLabeledVpn);
+                        }
+                    }
+                    MpUnreach::Ipv6Unicast { nlri } => {
+                        if nlri.is_empty() {
+                            current = Some(AddressType::Ipv6Unicast);
+                        }
+                    }
+                    MpUnreach::Ipv6Multicast { nlri } => {
+                        if nlri.is_empty() {
+                            current = Some(AddressType::Ipv6Multicast);
+                        }
+                    }
+                    MpUnreach::Ipv6NlriMplsLabels { nlri } => {
+                        if nlri.is_empty() {
+                            current = Some(AddressType::Ipv6NlriMplsLabels);
+                        }
+                    }
+                    MpUnreach::Ipv6MplsVpnUnicast { nlri } => {
+                        if nlri.is_empty() {
+                            return Some(AddressType::Ipv6MplsLabeledVpn);
+                        }
+                    }
+                    MpUnreach::L2Evpn { nlri } => {
+                        if nlri.is_empty() {
+                            return Some(AddressType::L2VpnBgpEvpn);
+                        }
+                    }
+                    MpUnreach::RouteTargetMembership { nlri } => {
+                        if nlri.is_empty() {
+                            current = Some(AddressType::RouteTargetConstrains);
+                        }
+                    }
+                    MpUnreach::Unknown { .. } => {
+                        // For unknown address families we assume it's not EoR, as they might have
+                        // different semantics defined.
+                        current = None;
+                    }
+                }
+            }
+        }
+        current
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        nlri::{
+            Ipv4MplsVpnUnicastAddress, Ipv4Unicast, LabeledIpv6NextHop, LabeledNextHop, MplsLabel,
+            RouteDistinguisher,
+        },
+        path_attribute::MpReach,
+    };
+
+    #[test]
+    fn test_end_of_rib() {
+        let ipv4_eor = BgpUpdateMessage::new(vec![], vec![], vec![]);
+        let ipv4_unicast_eor = BgpUpdateMessage::new(
+            vec![],
+            vec![PathAttribute::from(
+                true,
+                false,
+                false,
+                false,
+                PathAttributeValue::MpUnreach(MpUnreach::Ipv4Unicast { nlri: vec![] }),
+            )
+            .unwrap()],
+            vec![],
+        );
+        let ipv6_eor = BgpUpdateMessage::new(
+            vec![],
+            vec![PathAttribute::from(
+                true,
+                false,
+                false,
+                false,
+                PathAttributeValue::MpUnreach(MpUnreach::Ipv6Unicast { nlri: vec![] }),
+            )
+            .unwrap()],
+            vec![],
+        );
+        let ipv4_multicast_eor = BgpUpdateMessage::new(
+            vec![],
+            vec![PathAttribute::from(
+                true,
+                false,
+                false,
+                false,
+                PathAttributeValue::MpUnreach(MpUnreach::Ipv4Multicast { nlri: vec![] }),
+            )
+            .unwrap()],
+            vec![],
+        );
+        let ipv6_multicast_eor = BgpUpdateMessage::new(
+            vec![],
+            vec![PathAttribute::from(
+                true,
+                false,
+                false,
+                false,
+                PathAttributeValue::MpUnreach(MpUnreach::Ipv6Multicast { nlri: vec![] }),
+            )
+            .unwrap()],
+            vec![],
+        );
+
+        let with_nlri = BgpUpdateMessage::new(
+            vec![],
+            vec![],
+            vec![Ipv4UnicastAddress::new_no_path_id(
+                Ipv4Unicast::from_net("192.168.0.0/24".parse().unwrap()).unwrap(),
+            )],
+        );
+        let with_withdraw = BgpUpdateMessage::new(
+            vec![Ipv4UnicastAddress::new_no_path_id(
+                Ipv4Unicast::from_net("192.168.0.0/24".parse().unwrap()).unwrap(),
+            )],
+            vec![],
+            vec![],
+        );
+        let with_mp_reach = BgpUpdateMessage::new(
+            vec![],
+            vec![PathAttribute::from(
+                true,
+                false,
+                false,
+                true,
+                PathAttributeValue::MpReach(MpReach::Ipv4MplsVpnUnicast {
+                    next_hop: LabeledNextHop::Ipv6(LabeledIpv6NextHop::new(
+                        RouteDistinguisher::As2Administrator { asn2: 0, number: 0 },
+                        "fc00::1".parse().unwrap(),
+                    )),
+                    nlri: vec![Ipv4MplsVpnUnicastAddress::new_no_path_id(
+                        RouteDistinguisher::As2Administrator { asn2: 1, number: 1 },
+                        vec![MplsLabel::new([0, 65, 0]), MplsLabel::new([0, 65, 1])],
+                        Ipv4Unicast::from_net("192.168.1.0/24".parse().unwrap()).unwrap(),
+                    )],
+                }),
+            )
+            .unwrap()],
+            vec![],
+        );
+
+        let with_more_than_one_mp_unreach = BgpUpdateMessage::new(
+            vec![],
+            vec![
+                PathAttribute::from(
+                    true,
+                    false,
+                    false,
+                    false,
+                    PathAttributeValue::MpUnreach(MpUnreach::Ipv4Unicast { nlri: vec![] }),
+                )
+                .unwrap(),
+                PathAttribute::from(
+                    true,
+                    false,
+                    false,
+                    false,
+                    PathAttributeValue::MpUnreach(MpUnreach::Ipv6Multicast { nlri: vec![] }),
+                )
+                .unwrap(),
+            ],
+            vec![],
+        );
+
+        assert_eq!(with_nlri.end_of_rib(), None);
+        assert_eq!(with_withdraw.end_of_rib(), None);
+        assert_eq!(with_mp_reach.end_of_rib(), None);
+        assert_eq!(with_more_than_one_mp_unreach.end_of_rib(), None);
+
+        assert_eq!(ipv4_eor.end_of_rib(), Some(AddressType::Ipv4Unicast));
+        assert_eq!(
+            ipv4_unicast_eor.end_of_rib(),
+            Some(AddressType::Ipv4Unicast)
+        );
+        assert_eq!(ipv6_eor.end_of_rib(), Some(AddressType::Ipv6Unicast));
+        assert_eq!(
+            ipv4_multicast_eor.end_of_rib(),
+            Some(AddressType::Ipv4Multicast)
+        );
+        assert_eq!(
+            ipv6_multicast_eor.end_of_rib(),
+            Some(AddressType::Ipv6Multicast)
+        );
     }
 }
