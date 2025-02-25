c5f4939bbeb59dbd6b4473422ae256770cc9c967
Ahmed Elhassany <Ahmed.Elhassany@swisscom.com>
2025-02-25 11:51:17 +0100

    Add placeholder aggregation actor

--- crates/collector/src/flow/mod.rs
+++ crates/collector/src/flow/mod.rs
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub mod aggregation;
 pub mod config;
 
 use apache_avro::types::{Value as AvroValue, ValueKind as AvroValueKind};
