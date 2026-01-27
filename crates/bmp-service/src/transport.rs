ffabd337e02c6296dbaeab8f01ae573b85265810
Ahmed Elhassany <Ahmed.Elhassany@swisscom.com>
2026-01-27 15:33:02 +0100

    Reduce unused deps and features

--- crates/bmp-service/src/transport.rs
+++ crates/bmp-service/src/transport.rs
@@ -139,7 +139,7 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use futures::StreamExt;
+    use futures_util::StreamExt;
     use netgauze_bmp_pkt::codec::{BmpCodec, BmpCodecDecoderError};
     use netgauze_bmp_pkt::iana::UndefinedBmpVersion;
     use netgauze_bmp_pkt::v3::*;
