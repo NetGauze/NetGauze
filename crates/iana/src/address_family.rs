0a7ace2cb729717d9557fd733f7eee145eddaae0
Ahmed Elhassany <Ahmed.Elhassany@swisscom.com>
2026-04-20 20:56:43 +0200

    Add display implementation for AddressType

--- crates/iana/src/address_family.rs
+++ crates/iana/src/address_family.rs
@@ -397,7 +397,9 @@ impl TryFrom<u8> for SubsequentAddressFamily {
 /// Since not all [`AddressFamily`] and [`SubsequentAddressFamily`] are valid
 /// combinations, this enum defines a set of valid combination to ensure only
 /// valid AFI/SAFI are used at compile time.
-#[derive(Copy, Clone, PartialEq, Eq, Debug, Hash, Serialize, Deserialize)]
+#[derive(
+    Copy, Clone, PartialEq, Eq, Debug, Hash, strum_macros::Display, Serialize, Deserialize,
+)]
 #[cfg_attr(feature = "fuzz", derive(arbitrary::Arbitrary))]
 pub enum AddressType {
     Ipv4Unicast,
